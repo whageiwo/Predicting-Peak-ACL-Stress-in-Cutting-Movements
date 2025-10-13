3cdf6fc0c8dc76816a8033192522fa206669c11e
whageiwo <2716961457@qq.com>
2025-10-13 09:45:58 +0800

    Update lessweb.py

--- CODE/lessweb.py
+++ CODE/lessweb.py
@@ -90,7 +90,7 @@ force_plot = shap.force_plot(
 )
 
 html_code = f"""
-<div style='width:100%; display:flex; justify-content:center; margin-top:20px;'>
+<div style='width:100%; display:flex; justify-content:center; margin-top:5px;'>
     <div style='width:100%; overflow-x:visible; border:1px solid #ddd; padding:5px;'>
         <head>{shap.getjs()}</head>
         {force_plot.html()}
@@ -98,5 +98,6 @@ html_code = f"""
 </div>
 """
 components.html(html_code, height=400)
+
 
 
